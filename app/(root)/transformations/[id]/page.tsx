d4bc8d04b0386abc79b74d7f64f1e38404190e27
Shivom Singh <shivompal676@gmail.com>
2025-05-30 15:44:24 +0530

    commit2

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+
+    //
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TransformationsPage
+
+//
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    }
 };
-
 export default nextConfig;
+
+// module.exports = {
+//   reactStrictMode: false,
+// };
+// export default nextConfig;
